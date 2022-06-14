78fad5ab2456d2c1385d431592c6baf26171c255
iragh <83887242+iragh@users.noreply.github.com>
2022-06-14 00:04:53 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "ec2-prod" {
     type = "ssh"
     host = aws_instance.ec2-prod.public_ip
     private_key = file("~/testec2.pem")
-    echo AKIAIOSFODNN7EXAMPLX > filecredentialtemp.txt
     user = "ec2-user"
   }
 
